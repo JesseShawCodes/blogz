86c743849b50cc12c3fa894180827f61e25e56da
Jesse Shaw <JesseShawCodes@gmail.com>
2019-01-12 11:02:10 -0500

    Updated Main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import cgi
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:EFG7ziwMfYl1Tds1@localhost:8889/blogz'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return "Greetings"
+
+if __name__ == "__main__":
+    app.run()
